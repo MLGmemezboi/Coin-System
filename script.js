6597fcb2211b4196a6677b20ccb5d47ec264ea5e
Existable <64483586+MLGmemezboi@users.noreply.github.com>
2020-10-31 17:05:04 -0400

    script.js

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-var coins = 0;
+let coins = 0;
 
-//payment function
-let buy2 = document.createElement("button");
-buy2.textContent = "pay";
-buy2.addEventListener("click", () => {
-  if (coins === 50) {
-    let coingenerator2 = document.createElement("button");
-    coingenerator2.textContent = "+1 coins";
-    coingenerator2.addEventListener("click", () => {
-      coins + 2;
-    });
-  }
-});
-document.body.appendChild(buy2);
-//Click this to get more coins in the minigame thing
-let coingenerator = document.createElement("button");
-coingenerator.textContent = "+1 coin";
-coingenerator.addEventListener("click", () => {
-  coins + 1;
-});
-document.body.appendChild(coingenerator);
 
-//shows the amount of coins you farmed
-let coinamt = document.createElement("p");
-coinamt.textContent = coins + " coins";
-document.body.appendChild(coinamt);
+let coinamt = document.createElement('h1')
+coinamt.textContent = coins
+document.body.appendChild(coinamt)
+
+let coin1 = document.createElement('button')
+coin1.textContent = '+1'
+coin1.addEventListener('click', () => {
+coins++
+coinamt.textContent = coins;
+})
+
+document.body.appendChild(coin1)
